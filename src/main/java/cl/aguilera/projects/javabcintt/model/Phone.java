e1f1e838dd3f1076609e70b95bfebcba6291feaf
Jessy Aguilera <jessy.aguilera.lopez@gmail.com>
2024-07-08 16:49:06 -0400

    generar repositorio de persistencia y configurar db h2

---
+++ src/main/java/cl/aguilera/projects/javabcintt/model/Phone.java
@@ -0,0 +1,27 @@
+package cl.aguilera.projects.javabcintt.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phones")
+@Data
+@NoArgsConstructor
+public class Phone {
+    @Id
+    @GeneratedValue(generator = "uuid-hibernate-generator")
+    @GenericGenerator(name = "uuid-hibernate-generator", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+    private Long number;
+    private int citycode;
+    private String countrycode;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+}
---
+++ src/main/java/cl/aguilera/projects/javabcintt/model/User.java
@@ -0,0 +1,29 @@
+package cl.aguilera.projects.javabcintt.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(generator = "uuid-hibernate-generator")
+    @GenericGenerator(name = "uuid-hibernate-generator", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+    private Date created;
+    private Date lastLogin;
+    private String token;
+    private Boolean isActive;
+    private String name;
+    private String email;
+    private String password;
+}
